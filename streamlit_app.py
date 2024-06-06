d3ab401d11455650d556b7a9258f63d4b8df9262
anadenisseg <ana.g.galindo@oracle.com>
2024-06-05 18:27:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,12 @@ import pandas as pd
 import streamlit as st
 
 st.header("PROJECT - TEAM 5")
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-"""
-Cachar lo que meta en un input text
-nombre de la variable
-"""
-input = st.text_input("NEW VALUE")
-st.write("My new value: ", input)
 
+"""nombre de la variable"""
+input = st.text_input("NEW NAME")
+st.write("My new name: ", input)
+
+"""buscar en el json el acceso/permisos que tiene la persona y mostrarlas"""
 
 
 file = st.file_uploader('File uploader')
