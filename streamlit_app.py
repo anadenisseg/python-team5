44beeafe343e1a829a2a642e18742b60dad8ff1d
anadenisseg <ana.g.galindo@oracle.com>
2024-06-11 18:46:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,110 @@
-import altair as alt
-import numpy as np
+import json
 import pandas as pd
 import streamlit as st
-import json
+import altair as alt
+import numpy as np
 
-st.header("PROJECT - TEAM 5")
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+    
+def print_and_write(msg):
+    print(msg)
+    st.write(msg)
 
-#buscar en el json el acceso/permisos que tiene la persona y mostrarlas
-def read_file(file_name):
-    df = pd.DataFrame()
-    df = pd.read_csv(file_name)
-    #columns_info = {"columns": list(df.columns)}
-    #print(json.dumps(columns_info, indent=4))
-    for col in df.columns:
-        print(col)
+def get_valid_name(name):
+    for name_ in users['users']:
+        if name_['username'].lower() == name.lower():
+            return name_
+    else:
+        print_and_write('Invalid Name: Please provide a name inside the database')
 
-netflix = "netflix.csv"
-IMDB = "IMDb Top TV Series.csv"
-read_file(netflix)
-read_file(IMDB)
+def get_user_access(name):
+    return get_valid_name(name)['access']
 
-users_data  = json.load(open("users.json", "r"))
-roles_data  = json.load(open("roles.json", "r"))
-dd_data  = json.load(open("dd.json", "r"))
+def get_user_role(name):
+    return get_valid_name(name)['role']
 
-def get_info_by_name(name):
-  user_record = next((user for user in users_data['users'] if user['name'] == name), None)
+def get_user_dds(name):
+    return get_valid_name(name)['DDs']
 
-  if user_record:
-    user_roles = user_record['roles']
-    user_roles_data = roles_data.get(user_roles, {})
-    user_info = {field: dd_data[field] for field in user_roles_data.get('campos', [])}
-    #print(user_info)
-    user_json = {
-      "name": user_record['name'],
-      "roles": user_roles,
-      "info": user_info
-    }
-    return user_json
-  else:
-    return None
+def get_dds(name):
+    return dd
 
-#nombre de la variable
-name = st.text_input("NEW NAME")
-st.write("My new name: ", name)
-user_info = get_info_by_name(name)
+def get_user_info(name):
+    user_info = {}
 
-if user_info:
-     #print(user_info)
-  print(json.dumps(user_info, indent=4))
-else:
-  print(f"No hay info de '{name}'.")
+    access = get_user_access(name)
+    role = get_user_role(name)
+    dds = get_user_dds(name)
 
-file = st.file_uploader('File uploader')
-nd = pd.read_csv(file)
-st.write(nd)
+    tag = get_user_tag(name)
+    des = get_user_des(name)
+
+    user_info['name']   = name
+    user_info['access'] = access
+    user_info['role']   = role
+    user_info['dd']     = des
+
+    return json.dumps(user_info, indent=4)
+
+def get_user_tag(name):
+    user = get_valid_name(name)
+    role = user['role']
+
+    for each in roles['roles']:
+        if each['role'] == role:
+            tag = each['tag']
+            return tag
+
+def get_user_des(name):
+    dds_obj = get_dds(name)
+    user_tag = get_user_tag(name)
+    result = {}
+    for each in dds_obj:
+        result[each] = dds_obj[each][user_tag]
+    return result
+
+
+# Carga de datos
+users = load_json('../data/users.json')
+roles = load_json('../data/roles.json')
+dd    = load_json('../data/DD.json')
+df_netflix = pd.read_csv(r'C:\Users\slahud\Desktop\Cursos Python Oracle\Python Avanzado\Proyecto\data\netflix.csv')
+df_imdb = pd.read_csv(r'C:\Users\slahud\Desktop\Cursos Python Oracle\Python Avanzado\Proyecto\data\IMDb Top TV Series.csv')
+
+try:
+    st.header("PROJECT - TEAM 5")
+    name = st.text_input("INSERT NEW NAME")
+    st.write("MY NAME IS: ", name)
+
+    user_info_st = get_user_info(name)
+    ddd          = get_user_dds(name)
+    desc         = get_user_des(name)
+
+    net_nick_col_names  = []
+    imdb_nick_col_names = []
+    net_gold_col_names  = []
+    imdb_gold_col_names = []
+
+    for each in ddd:
+        for file in desc[each]:
+            if 'netflix' in file['DBD'].lower():
+                net_nick_col_names.append(file['nickname'])
+                net_gold_col_names.append(file['golden_name'].lower())
+
+            elif 'imdb'  in file['DBD'].lower():
+                imdb_nick_col_names.append(file['nickname'])
+                imdb_gold_col_names.append(file['golden_name'])
+
+    df_netflix = df_netflix.rename(columns=dict(zip(net_gold_col_names, net_nick_col_names)))
+    st.write("Netflix Access:")
+    st.write(df_netflix[net_nick_col_names])
+
+    df_imdb = df_imdb.rename(columns=dict(zip(imdb_gold_col_names, imdb_nick_col_names)))
+    st.write("IMDBs Access:")
+    st.write(df_imdb[imdb_nick_col_names])
+
+except:
+    if name is not None and name != "":
+        st.write(f"NO INFORMATION OF NAME '{name}'.")
